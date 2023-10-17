040853892c05385bdaf661fb7ff8649207804a23
Ashish Garg <gargcreation1992@gmail.com>
2023-10-17 11:45:09 +0530

    migrate adform alias to use new pattern (#3180)

--- openrtb_ext/bidders.go
+++ openrtb_ext/bidders.go
@@ -23,7 +23,6 @@ var coreBidderNames []BidderName = []BidderName{
 	BidderAceex,
 	BidderAcuityAds,
 	BidderAdf,
-	BidderAdform,
 	BidderAdgeneration,
 	BidderAdhese,
 	BidderAdkernel,
@@ -316,7 +315,6 @@ const (
 	BidderAceex             BidderName = "aceex"
 	BidderAcuityAds         BidderName = "acuityads"
 	BidderAdf               BidderName = "adf"
-	BidderAdform            BidderName = "adform"
 	BidderAdgeneration      BidderName = "adgeneration"
 	BidderAdhese            BidderName = "adhese"
 	BidderAdkernel          BidderName = "adkernel"
