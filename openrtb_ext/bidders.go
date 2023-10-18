f477b407d430018958468921cdb9078fa49533a9
Ashish Garg <gargcreation1992@gmail.com>
2023-10-18 18:39:08 +0530

    migrate viewdeos alias to use new pattern (#3192)

--- openrtb_ext/bidders.go
+++ openrtb_ext/bidders.go
@@ -203,7 +203,6 @@ var coreBidderNames []BidderName = []BidderName{
 	BidderVideoByte,
 	BidderVideoHeroes,
 	BidderVidoomy,
-	BidderViewdeos,
 	BidderVisibleMeasures,
 	BidderVisx,
 	BidderVox,
@@ -492,7 +491,6 @@ const (
 	BidderVideoByte         BidderName = "videobyte"
 	BidderVideoHeroes       BidderName = "videoheroes"
 	BidderVidoomy           BidderName = "vidoomy"
-	BidderViewdeos          BidderName = "viewdeos"
 	BidderVisibleMeasures   BidderName = "visiblemeasures"
 	BidderVisx              BidderName = "visx"
 	BidderVox               BidderName = "vox"
